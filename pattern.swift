e20b71e44ab4492bbf953ffcc89af5e3655eca57
Tony5897 <tony.martinez5897@gmail.com>
2022-03-23 02:03:11 -0700

    Working through swift output capabilities.

---
+++ pattern.swift
@@ -0,0 +1,32 @@
+print("Hello")
+print("Goodbye")
+
+// This is how you comment when working with Swift.
+// Single line comments.
+/* Multi-line comments.
+// Swift reads line by line. The example above shows that with 'hello'(next line)'goodbye'
+// Use Proper Syntax.
+
+// Printing an emoji
+
+print("🚙💨 ")
+
+// Print with multiple line.
+
+print("      1")
+print("    2 3")
+print("   4 5 6")
+print("7 8 9 10")
+
+//
+
+// 🚙  just another example commenting.
+
+//🚙
+print("tony martinez")
+
+print("Dear Self, you learned a little bit Swift programming today. You will continue to see if it is a proper fitting programming language to keep in your intellectual tool")
+
+print("This...is...fine")
+
+
